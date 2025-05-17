1350bb3eb0f6554fac62fbc2887c868b139b7458
MuhammadAmin02 <muhammadamininto011@gmail.com>
2025-05-17 15:13:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let a = prompt(" ismingizni kiriting")
+let b = +prompt("parolingizni kiriting")
+let c = prompt(" telefon raqamingizni kiriting")
+let d = confirm("kiritgan parolingizni tekshiring")
+
+
+alert(`   ${ typeof a}  \n ${typeof b} \n${ typeof c } \n ${typeof d} `)
+
+
+
+ //console.log(typeof b)
+
+//console.log(typeof c)
+
+//console.log( typeof d)
+
+//console.log(typeof alert)
+
+
